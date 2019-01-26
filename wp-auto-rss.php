aa4dc788b3f1e78486c03047f1a88ccc8ba1466e
Dan Robinson <dlrobinson@protonmail.com>
2019-01-25 20:53:17 -0500

    add header to main file

---
+++ wp-auto-rss.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @package AutoRSS
+ * @author Dan Robinson
+ * @copyright 2019 Dan Robinson
+ * @license MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name: Auto RSS
+ * Plugin URI: https://github.com/danrobinson78/wp-auto-rss
+ * Description: autogenerate an RSS feed file based on current posts
+ * Author: Dan Robinson
+ * Version: 1.0
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ */
+
+?>
